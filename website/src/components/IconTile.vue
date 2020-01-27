7ee6c6ce225a4f4f6b893d0903466d450186f07b
Peter Sherk <psherk95@gmail.com>
2020-01-26 19:47:32 -0500

    finished adding tiles part 2

---
+++ website/src/components/IconTile.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="level is-mobile">
+        <div class="level-left">
+            <div class="level-item">
+                <i :class="icon" class="fas fa-3x"></i>
+            </div>
+            <div class="level-item">
+                <h1 class="title">{{tileTitle}}</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class IconTile extends Vue {
+  @Prop() private tileTitle!: string;
+  @Prop({ default: 'fa-exclamation-triangle' }) private icon!: string;
+}
+</script>
+
+<style lang="scss">
+    .icontile__title-buffer {
+        margin-left: 1rem;
+    }
+</style>
---
+++ website/src/views/Banner.vue
@@ -0,0 +1,55 @@
+<template>
+    <section class="hero is-fullheight-with-navbar">
+    <!-- Hero content: will be in the middle -->
+    <div class="hero-body">
+        <div class="container">
+            <div class="columns">
+                <div class="column">
+                    <h1 class="title">
+                        Welcome
+                    </h1>
+                    <h2 class="subtitle">
+                        This website showcases my many passions, from coding to cooking to music.
+                    </h2>
+                </div>
+                <div class="column">
+                    <div class="tile is-ancestor">
+                        <div class="tile is-6 is-vertical is-parent">
+                            <div class="tile is-child box">
+                                <IconTile tileTitle="Coding" icon="fa-laptop-code"/>
+                            </div>
+                            <div class="tile is-child box">
+                                <IconTile tileTitle="Cooking" icon="fa-cookie-bite"/>
+                            </div>
+                            <div class="tile is-child box">
+                                <IconTile tileTitle="Music" icon="fa-compact-disc"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </section>
+</template>
+
+<script lang="ts">
+// @ is an alias to /src
+import IconTile from '@/components/IconTile.vue'
+
+export default {
+  name: 'banner',
+  components: {
+    IconTile
+  }
+}
+</script>
+
+<style lang="scss">
+    .welcome__label-buffer {
+        margin-left: 1rem;
+    }
+    .welcome-larger {
+        font-size: 2rem;
+    }
+</style>

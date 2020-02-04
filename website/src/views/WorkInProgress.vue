4b93b8e249ee3087da64c2cebdace2247156eb04
Peter Sherk <psherk95@gmail.com>
2020-02-04 14:04:16 -0500

    Adding back in toolbox font-awesome icon

--- website/src/views/WorkInProgress.vue
+++ website/src/views/WorkInProgress.vue
@@ -2,7 +2,7 @@
     <div class="hero is-fullheight-with-navbar">
       <div class="hero-body">
         <div class="container has-text-centered">
-          <i style="width: 150px; height: 150px; color:#FF9436;" class="fas fa-toolbox"></i>
+          <font-awesome-icon :icon="['fas', 'toolbox']" style="width: 150px; height: 150px; color:#FF9436;" />
           <h1 class="title">Page under development</h1>
           <h2 class="subtitle">New content is always updated as soon as possible!</h2>
         </div>

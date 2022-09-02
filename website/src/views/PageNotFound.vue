9d82948b81972287a733de19d26d91cfb9bd8d80
Peter Sherk <psherk95@gmail.com>
2022-09-02 17:46:48 -0400

    Update about and resume, fix issue with resume routing

--- website/src/views/PageNotFound.vue
+++ website/src/views/PageNotFound.vue
@@ -1,4 +1,5 @@
 <template>
+  <meta name="robots" content="noindex"/> <!-- Blocks indexing on most webcrawlers like Google -->
   <GenericError errorMessage="Oops! Page Not Found." errorPicturePath="img/moose_404.jpeg"
     altText="moose 404" navigationText="Go Home" navigationPath="/"/>
 </template>

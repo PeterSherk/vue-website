6e789fdd17e5e62d8247c9d9dc5d49b00b116e8f
Peter Sherk <psherk95@gmail.com>
2022-09-10 12:08:44 -0400

    moved images to top dir and added api reference for docs and images

--- website/src/views/PageNotFound.vue
+++ website/src/views/PageNotFound.vue
@@ -1,6 +1,6 @@
 <template>
   <meta name="robots" content="noindex"/> <!-- Blocks indexing on most webcrawlers like Google -->
-  <GenericError errorMessage="Oops! Page Not Found." errorPicturePath="img/moose_404.jpeg"
+  <GenericError errorMessage="Oops! Page Not Found." errorPicturePath="https://api.petersherk.com/img/moose_404.jpeg"
     altText="moose 404" navigationText="Go Home" navigationPath="/"/>
 </template>
 

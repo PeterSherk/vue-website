9a1efe749287c949cc84076de1b1cd78d1ad9bd1
Peter Sherk <psherk95@gmail.com>
2024-04-30 00:57:08 -0400

    updated redis host and other small details

--- api/src/app/db/redis.js
+++ api/src/app/db/redis.js
@@ -1,7 +1,11 @@
 import { createClient } from 'redis';
 import { logger } from '../../configs/logger';
 
-const redis = createClient()
+const redisHost = process.env.REDIS_HOST || 'localhost';
+
+const redis = createClient({
+  url: `redis://${redisHost}:6379`
+})
   .on('error', err => logger.log({
     level: 'error',
     message: `Redis Client Error

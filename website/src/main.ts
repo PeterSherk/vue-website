f3e07cc7f41faa507812b957b8fa30f11dc9f05b
Peter Sherk <psherk95@gmail.com>
2024-07-12 09:27:52 -0400

    Feature/spotify status bar (#49)
    * upgraded to bulma 1.0, fixed issues with migration
    
    * added music streaming page

--- website/src/main.ts
+++ website/src/main.ts
@@ -9,14 +9,14 @@ import {
   faChevronRight
 } from '@fortawesome/free-solid-svg-icons'
 import { faEnvelope, faCalendarAlt } from '@fortawesome/free-regular-svg-icons'
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin, faGithub, faSpotify } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import '@/assets/index.scss'
 
 library.add(
   faLaptopCode, faCookieBite, faCompactDisc, faToolbox, faChevronRight,
   faEnvelope, faCalendarAlt,
-  faLinkedin, faGithub
+  faLinkedin, faGithub, faSpotify
 )
 
 const app = createApp(App)

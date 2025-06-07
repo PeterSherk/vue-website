05e3ee77c642af7db57fb232970c7651646a18a2
Peter Sherk <psherk95@gmail.com>
2025-06-07 17:19:13 -0400

    formatter

--- website/src/components/Coding.spec.ts
+++ website/src/components/Coding.spec.ts
@@ -7,38 +7,38 @@ import GenericError from './GenericError.vue'
 let codingWrapper: VueWrapper
 
 describe('Coding.vue tests', () => {
-    beforeEach(() => {
-        codingWrapper = mount(Coding, {
-            components: {
-                GenericError,
-                Loader
-            },
-            global: {
-                components: {
-                    'router-link': RouterLinkStub
-                },
-                stubs: {
-				    FontAwesomeIcon: true
-			    }
-            }
-        })
-    })
+	beforeEach(() => {
+		codingWrapper = mount(Coding, {
+			components: {
+				GenericError,
+				Loader
+			},
+			global: {
+				components: {
+					'router-link': RouterLinkStub
+				},
+				stubs: {
+					FontAwesomeIcon: true
+				}
+			}
+		})
+	})
 
-    test('displays error message when API call fails', async () => {
-        await codingWrapper.setData({ errored: true, loading: false })
-        await codingWrapper.vm.$nextTick()
-        expect(codingWrapper.findComponent({ name: 'GenericError' }).exists()).toBe(true)
-    })
+	test('displays error message when API call fails', async () => {
+		await codingWrapper.setData({ errored: true, loading: false })
+		await codingWrapper.vm.$nextTick()
+		expect(codingWrapper.findComponent({ name: 'GenericError' }).exists()).toBe(true)
+	})
 
-    test('displays projects when API call succeeds', async () => {
-        const projects = [
-            { id: 1, name: 'Project A', company: 'Company X', year: 2022 },
-            { id: 2, name: 'Project B', company: 'Company Y', year: 2023 }
-        ]
-        await codingWrapper.setData({ projects, loading: false })
-        await codingWrapper.vm.$nextTick()
-        expect(codingWrapper.findAll('.card').length).toBe(2)
-        expect(codingWrapper.text()).toContain('Project A')
-        expect(codingWrapper.text()).toContain('Project B')
-    })
+	test('displays projects when API call succeeds', async () => {
+		const projects = [
+			{ id: 1, name: 'Project A', company: 'Company X', year: 2022 },
+			{ id: 2, name: 'Project B', company: 'Company Y', year: 2023 }
+		]
+		await codingWrapper.setData({ projects, loading: false })
+		await codingWrapper.vm.$nextTick()
+		expect(codingWrapper.findAll('.card').length).toBe(2)
+		expect(codingWrapper.text()).toContain('Project A')
+		expect(codingWrapper.text()).toContain('Project B')
+	})
 })
--- website/src/components/Cooking.spec.ts
+++ website/src/components/Cooking.spec.ts
@@ -4,21 +4,21 @@ import Cooking from './Cooking.vue'
 import WorkInProgress from '@/views/WorkInProgress.vue'
 
 describe('Cooking.vue', () => {
-    const wrapper = mount(Cooking, {
-      global: {
-        components: {
-          WorkInProgress
-        },
-        stubs: {
-          FontAwesomeIcon: true
-        }
-      }
-    })
-  test('renders the component', () => {
-    expect(wrapper.exists()).toBe(true)
-  })
+	const wrapper = mount(Cooking, {
+		global: {
+			components: {
+				WorkInProgress
+			},
+			stubs: {
+				FontAwesomeIcon: true
+			}
+		}
+	})
+	test('renders the component', () => {
+		expect(wrapper.exists()).toBe(true)
+	})
 
-  test('renders WorkInProgress component', () => {
-    expect(wrapper.findComponent(WorkInProgress).exists()).toBe(true)
-  })
+	test('renders WorkInProgress component', () => {
+		expect(wrapper.findComponent(WorkInProgress).exists()).toBe(true)
+	})
 })
